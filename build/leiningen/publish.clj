1e7daf06adb48b22b2ad6e25b4d4f5d4213509bd
Daniel Truemper <truemped@gmail.com>
2016-06-07 09:09:03 +0200

    Rename circle to publish

--- build/leiningen/publish.clj
+++ build/leiningen/publish.clj
@@ -2,6 +2,6 @@
   (:require [leiningen.run :as run]
             [leiningen.rsync :as rsync]))
 
-(defn circle [project & args]
+(defn publish [project & args]
   (run/run project)
   (rsync/rsync project "resources/public/" "master"))
