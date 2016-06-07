1fda73457350aeead5a81d88a20871c24bade756
Daniel Truemper <truemped@gmail.com>
2016-06-07 13:21:37 +0200

    Removing lein command for publishing

--- build/leiningen/publish.clj
+++
@@ -1,7 +0,0 @@
-(ns leiningen.publish
-  (:require [leiningen.run :as run]
-            [leiningen.rsync :as rsync]))
-
-(defn publish [project & args]
-  (run/run project)
-  (rsync/rsync project "resources/public/" "master"))
